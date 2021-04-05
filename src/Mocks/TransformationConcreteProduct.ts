4218d3a67e7e2bc4c34981e94c2f96e81067968a
Victor Turra <victor.florencio@univem.edu.br>
2021-04-04 22:34:29 -0300

    Refactored the transformation rate assignment

--- src/Mocks/TransformationConcreteProduct.ts
+++ src/Mocks/TransformationConcreteProduct.ts
@@ -1,8 +1,6 @@
 import { TransformationMatrix } from '../Transformation/TransformationMatrix';
 
 export class TransformationConcreteProduct extends TransformationMatrix {
-  public rate: { xAxis: number, yAxis: number };
-
   public constructor(xAxisRate: number, yAxisRate: number) {
     super([
       [1, 0, 0],
@@ -10,14 +8,7 @@ export class TransformationConcreteProduct extends TransformationMatrix {
       [0, 0, 1],
     ]);
 
-    this.rate = {
-      xAxis: xAxisRate,
-      yAxis: yAxisRate,
-    };
-  }
-
-  assignRate() {
-    this.values[2][0] = this.rate.xAxis;
-    this.values[2][1] = this.rate.yAxis;
+    this.values[2][0] = xAxisRate;
+    this.values[2][1] = yAxisRate;
   }
 }
--- src/Transformation/Transformation.ts
+++ src/Transformation/Transformation.ts
@@ -5,16 +5,16 @@ export abstract class Transformation {
   public abstract createTransformationMatrix(): TransformationMatrix;
 
   private createPointHomogeneosCoordinate(point: [number, number]) {
+    const [xCoordinate, yCoordinate] = point;
+
     return new Matrix(1, 3, [
-      [point[0], point[1], 1],
+      [xCoordinate, yCoordinate, 1],
     ]);
   }
 
   public apply(point: [number, number]) {
     const transformationMatrix = this.createTransformationMatrix();
     const homogeneousPoint = this.createPointHomogeneosCoordinate(point);
-
-    transformationMatrix.assignRate();
 
     const result = Matrix.multiplyTwoFactors([homogeneousPoint, transformationMatrix]);
     const transformedPoint = [result.values[0][0], result.values[0][1]];
