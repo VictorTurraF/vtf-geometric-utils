c83f648706bea05fa77946c12fe54356b6b331f9
Victor Turra <victor.florencio@univem.edu.br>
2021-04-06 23:07:00 -0300

    Created the test case for the other transformations

--- src/Domain/Rotation/Rotation.test.ts
+++ src/Domain/Rotation/Rotation.test.ts
@@ -13,4 +13,18 @@ describe('Rotation transformation class', () => {
 
     expect(rotation.apply([5, 1])).toEqual([1, -5]);
   });
+
+  it('should be applying to multiple points', () => {
+    const rotation = RotationFactory.createRotationTransformation();
+
+    const rotationResult = rotation.applyToPoints([
+      [5, 0],
+      [-5, 0],
+    ]);
+
+    expect(rotationResult[0][0]).toBeCloseTo(2.5);
+    expect(rotationResult[0][1]).toBeCloseTo(-4.3301);
+    expect(rotationResult[1][0]).toBeCloseTo(-2.5);
+    expect(rotationResult[1][1]).toBeCloseTo(4.3301);
+  });
 });
