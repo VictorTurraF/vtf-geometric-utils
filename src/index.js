20eebfd73c474031ba55ca428d7e5a7d95607c35
Victor Turra <victor.florencio@univem.edu.br>
2021-04-07 23:36:16 -0300

    Added helper comments

--- src/index.js
+++ src/index.js
@@ -10,14 +10,17 @@ const squarePoints = [
   [4, 4],
 ];
 
+// Rotação de um objeto
 const rotation = new SelfRotation(45);
 const rotationResult = rotation.applyToPoints(squarePoints);
 console.log('Resultado da rotação do quadrado', rotationResult);
 
+// Escalonamento de um objeto
 const scaling = new SelfScaling(2, 2);
 const scalingResult = scaling.applyToPoints(squarePoints);
 console.log('Resultado do escalonamento do quadrado', scalingResult);
 
+// Translação de um objeto
 const translation = new Translation(1, 2);
 const translationResult = translation.applyToPoints(squarePoints);
 console.log('Resultado da tranlação do quadrado', translationResult);
